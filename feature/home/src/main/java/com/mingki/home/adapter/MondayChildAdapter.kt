08f6651bb5cb03a1417144f1d3d4eb29e507c4ea
MinKiCho <cmk11032@naver.com>
2023-11-28 02:55:45 +0900

    body settings

--- feature/home/src/main/java/com/mingki/home/adapter/MondayChildAdapter.kt
+++ feature/home/src/main/java/com/mingki/home/adapter/MondayChildAdapter.kt
@@ -1,19 +1,17 @@
 package com.mingki.home.adapter
 
-import android.util.Log
 import com.mingki.home.BR
 import com.mingki.home.R
 import com.mingki.model.TopicItem
 import com.mingki.ui.BaseListAdapter
 import com.mingki.ui.BaseViewHolder
 
-class TopicChildAdapter() : BaseListAdapter<TopicItem>() {
+class MondayChildAdapter  : BaseListAdapter<TopicItem>()  {
+
     override fun onBindViewHolder(holder: BaseViewHolder<TopicItem>, position: Int) {
         super.onBindViewHolder(holder, position)
         holder.binding.setVariable(BR.item, getItem(position))
-        Log.d("topicAdapter child", getItem(position).artist)
     }
 
-
-    override fun getItemViewType(position: Int): Int = R.layout.item_topic_child
+    override fun getItemViewType(position: Int): Int = R.layout.item_child_monday
 }
---
+++ feature/home/src/main/java/com/mingki/home/adapter/MoodChildAdapter.kt
@@ -0,0 +1,17 @@
+package com.mingki.home.adapter
+
+import com.mingki.home.BR
+import com.mingki.home.R
+import com.mingki.model.TopicItem
+import com.mingki.ui.BaseListAdapter
+import com.mingki.ui.BaseViewHolder
+
+class MoodChildAdapter  : BaseListAdapter<TopicItem>()  {
+
+    override fun onBindViewHolder(holder: BaseViewHolder<TopicItem>, position: Int) {
+        super.onBindViewHolder(holder, position)
+        holder.binding.setVariable(BR.item, getItem(position))
+    }
+
+    override fun getItemViewType(position: Int): Int = R.layout.item_child_mood
+}
