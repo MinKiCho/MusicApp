7a17ee075d624f0a367d3757e63b0d0de3b387bb
MinKiCho <cmk11032@naver.com>
2023-11-27 23:15:35 +0900

    viewpager settings

--- feature/home/src/main/java/com/mingki/home/HomeFragment.kt
+++ feature/home/src/main/java/com/mingki/home/HomeFragment.kt
@@ -1,51 +1,39 @@
 package com.mingki.home
 
-import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.ConcatAdapter
-import com.mingki.home.adapter.TopicAdapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
+import com.mingki.home.adapter.HomePagerAdapter
 import com.mingki.home.databinding.FragmentHomeBinding
-import com.mingki.model.Topic
-import com.mingki.model.TopicItem
-import com.mingki.ui.ViewPager2Animation
 import dagger.hilt.android.AndroidEntryPoint
+import java.lang.Math.abs
 
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
-    private lateinit var binding: FragmentHomeBinding
+    private var _binding: FragmentHomeBinding? = null
+    private val homeViewModel: HomeViewModel by viewModels()
+    private lateinit var homePagerAdapter: HomePagerAdapter
+    private val binding get() = _binding!!
+
+    //    private lateinit var _binding: FragmentHomeBinding
     private lateinit var concatAdapter: ConcatAdapter
-    private lateinit var topicAdapter: TopicAdapter
-    val topicItems = listOf<TopicItem>(
-        TopicItem("1", "몰라","몰라", "몰라", "몰라", "이미지가 있다고 가정"),
-        TopicItem("2", "몰라2", "몰라","몰라", "몰라", "이미지가 있다고 가정"),
-        TopicItem("3", "몰라3", "몰라","몰라", "몰라", "이미지가 있다고 가정"),
-        TopicItem("4", "몰라4", "몰라", "몰라","몰라", "이미지가 있다고 가정"),
-        TopicItem("5", "몰라5", "몰라", "몰라", "몰라","이미지가 있다고 가정"),
-        TopicItem("6", "몰라", "몰라", "몰라","몰라", "이미지가 있다고 가정"),
-        TopicItem("7", "몰라2", "몰라", "몰라", "몰라","이미지가 있다고 가정"),
-        TopicItem("8", "몰라3", "몰라", "몰라", "몰라","이미지가 있다고 가정"),
-        TopicItem("9", "몰라4", "몰라", "몰라", "몰라","이미지가 있다고 가정"),
-        TopicItem("10", "몰라5", "몰라", "몰라", "몰라","이미지가 있다고 가정")
-    )
-    val topic = listOf<Topic>(
-        Topic("1", "휴식이 필요할 떄 듣는 잔잔한 힐링인디", topicItems, Color.BLACK),
-        Topic("2", "여유롭고 차분하게, 앰비언트", topicItems, Color.WHITE),
-        Topic("3", "당신에게 감성의 끝을 선사할 팝", topicItems, Color.RED),
-        Topic("4", "가만히 듣다 보면 울컥하는 인디 록", topicItems, Color.GREEN),
-        Topic("5", "새벽을 채우는 몽환 바이브", topicItems, Color.BLUE)
-    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -53,21 +41,60 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = viewLifecycleOwner
         init()
+
     }
 
+
     private fun init() {
-        topicAdapter = TopicAdapter()
+        homePagerAdapter = HomePagerAdapter(requireActivity())
+
         binding.homeViewpager2.apply {
-            adapter = topicAdapter
-//            setPageTransformer(ViewPager2Animation())
+            adapter = homePagerAdapter
+            setPageTransformer(setAnimation)
+            offscreenPageLimit = 1
         }
 
-        topicAdapter.submitList(topic)
+        TabLayoutMediator(binding.homeTabLayout, binding.homeViewpager2) { tab, position ->
+        }.attach()
+
 
 //        concatAdapter = ConcatAdapter(
 //            topicAdapter
 //        )
 
 //        binding.homeRecyclerView.adapter = concatAdapter
+    }
+
+    private val setAnimation = ViewPager2.PageTransformer { view, position ->
+        val pageWidth = view.width
+        val title = view.findViewById<TextView>(R.id.topicTitle)
+        val playButton = view.findViewById<ImageView>(R.id.playButton)
+        val recyclerview = view.findViewById<RecyclerView>(R.id.topicRecyclerview)
+        val backgroundImage = view.findViewById<ImageView>(R.id.imageView)
+        val totalMusic = view.findViewById<TextView>(R.id.totalMusic)
+        val date = view.findViewById<TextView>(R.id.date)
+        val absPosition = abs(position)
+
+        if (position < -1) {
+            view.alpha = 1.0f
+        } else if (position <= 1) {
+            title.alpha = 1.0f - (absPosition)
+            totalMusic.alpha = 1.0f - (absPosition)
+            totalMusic.translationX = (position) * (pageWidth)
+            date.alpha = 1.0f - (absPosition)
+            date.translationX = (position) * (pageWidth)
+            playButton.translationX = (position) * (pageWidth / 2)
+            playButton.alpha = 1.0f - (absPosition * 2)
+            recyclerview.translationX = (position) * (pageWidth / 2)
+            recyclerview.alpha = 1.0f - (absPosition)
+            backgroundImage.translationX = (-position * (pageWidth/2))
+        } else {
+            view.alpha = 1.0f
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ feature/home/src/main/java/com/mingki/home/fragment/SecondFragment.kt
@@ -0,0 +1,108 @@
+package com.mingki.home.fragment
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mingki.ui.CustomLinearLayout
+import com.mingki.home.R
+import com.mingki.home.adapter.HomePagerChildAdapter
+import com.mingki.home.databinding.FragmentSecondBinding
+import com.mingki.model.TopicItem
+
+
+class SecondFragment : Fragment() {
+    private lateinit var binding : FragmentSecondBinding
+    private lateinit var homePagerChildAdapter: HomePagerChildAdapter
+    private var handler : Handler ?= null
+    private var runnable : Runnable?=null
+    val topicItems = listOf<TopicItem>(
+        TopicItem("1", "분홍신", "아이유", "몰라", "몰라", "이미지가 있다고 가정"),
+        TopicItem("2", "분홍신", "아이유","몰라", "몰라", "이미지가 있다고 가정"),
+        TopicItem("3", "분홍신", "아이유","몰라", "몰라", "이미지가 있다고 가정"),
+        TopicItem("4", "분홍신", "아이유","몰라", "몰라", "이미지가 있다고 가정"),
+        TopicItem("5", "분홍신", "아이유","몰라", "몰라", "이미지가 있다고 가정"),
+        TopicItem("6", "분홍신", "아이유","몰라", "몰라", "이미지가 있다고 가정"),
+        TopicItem("7", "분홍신", "아이유","몰라", "몰라", "이미지가 있다고 가정"),
+        TopicItem("8", "분홍신", "아이유","몰라", "몰라", "이미지가 있다고 가정"),
+        TopicItem("9", "분홍신", "아이유","몰라", "몰라", "이미지가 있다고 가정"),
+        TopicItem("10", "분홍신", "아이유", "몰라", "몰라", "이미지가 있다고 가정")
+    ).toList()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.topicTitle.text =  getString(R.string.second_title)
+
+        binding.totalMusic.text = String.format(getString(R.string.total_music), 15)
+        binding.date.text =  String.format(getString(R.string.date), "2023.1.1")
+
+        homePagerChildAdapter = HomePagerChildAdapter()
+
+        binding.topicRecyclerview.apply {
+            adapter = homePagerChildAdapter
+            layoutManager = CustomLinearLayout(requireContext())
+            addOnItemTouchListener(itemTouchListener)
+        }
+
+        homePagerChildAdapter.submitList(topicItems)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setAutoScroll()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if(handler != null){
+            handler!!.removeCallbacks(runnable!!)
+        }
+    }
+
+    private fun setAutoScroll() {
+        if(handler != null){
+            handler!!.removeCallbacks(runnable!!)
+        }
+        handler = Handler(Looper.getMainLooper())
+        runnable = object : Runnable {
+            override fun run() {
+                val linearLayoutManager =
+                    (binding.topicRecyclerview.layoutManager as LinearLayoutManager)
+                if (linearLayoutManager.findLastVisibleItemPosition() < (homePagerChildAdapter.itemCount - 1)) {
+                    linearLayoutManager.smoothScrollToPosition(
+                        binding.topicRecyclerview,
+                        RecyclerView.State(),
+                        linearLayoutManager.findLastCompletelyVisibleItemPosition() + 2
+                    )
+
+                } else if (linearLayoutManager.findLastCompletelyVisibleItemPosition() < (homePagerChildAdapter.itemCount - 1)) {
+
+                }
+                handler!!.postDelayed(this, 3000)
+            }
+        }
+        handler!!.postDelayed(runnable!!, 3000)
+    }
+    private val itemTouchListener = object : RecyclerView.SimpleOnItemTouchListener() {
+        override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+            return true
+        }
+    }
+}
